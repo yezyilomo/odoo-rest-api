dcf07849b845307ddc07cf27f487bb930d396ba6
yezy <yezileliilomo@hotmail.com>
2019-04-27 19:08:28 +0300

    Change API structure
    *Pass model as URL param(/api/<model>/)
    *Pass record id as URL param(/api/<model>/<record_id>)
    *Add a route to fetch single record(/api/<model>/<record_id>)
    *Format code according to PEP8

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,7 +7,6 @@ import werkzeug
 
 from odoo import http, api, _
 from odoo.http import request
-from odoo.addons.http_routing.models.ir_http import slug
 
 _logger = logging.getLogger(__name__)
 
@@ -16,7 +15,6 @@ try:
 except ImportError as err:
     _logger.debug(err)
 
-import pprint
 def flat_obj(obj, parent_obj, field_name):
     if isinstance(obj, datetime.datetime):
         return obj.strftime("%Y-%m-%d-%H-%M")
@@ -26,7 +24,8 @@ def flat_obj(obj, parent_obj, field_name):
         return obj.strftime("%H-%M-%S")
 
     if hasattr(parent_obj, "fields_get"):
-        field_type = parent_obj.fields_get(field_name)[field_name]["type"]
+        field = parent_obj.fields_get(field_name)[field_name]
+        field_type = field["type"]
         if field_type == "many2one":
             return obj.id
         if field_type in ["one2many", "many2many"]:
@@ -44,13 +43,10 @@ def nested_iter_obj(obj, parent_obj):
 
 class OdooAPI(http.Controller):
     @http.route(
-        '/api/', 
+        '/api/<string:model>', 
         auth='public', methods=['GET'], csrf=False)
-    def get_model_data(self, **params):
-        model = params["model"]
-
+    def get_model_data(self, model, **params):
         records = request.env[model].sudo().search([])
-
         if "query" in params:
             query = json.loads(params["query"])
         else:
@@ -80,10 +76,15 @@ class OdooAPI(http.Controller):
                 page_number = int(params["page"])
             else:
                 page_number = 1  # Default page Number
-
-            records = records[page_size*(page_number-1):page_number*page_size]
-            next_page = page_number+1 if 0<page_number+1 <= total_page_number else None
-            prev_page = page_number-1 if 0<page_number-1 <= total_page_number else None
+            start = page_size*(page_number-1)
+            stop = page_number*page_size
+            records = records[start:stop]
+            next_page = page_number+1 \
+                        if 0 < page_number + 1 <= total_page_number \
+                        else None
+            prev_page = page_number-1 \
+                        if 0 < page_number - 1 <= total_page_number \
+                        else None
 
         if "limit" in params:
             limit = int(params["limit"])
@@ -112,26 +113,59 @@ class OdooAPI(http.Controller):
         )
 
     @http.route(
-        '/api/', 
-        type='json', auth="public", methods=['POST'], website=True, csrf=False)
-    def post_model_data(self, **post):
-        model = post["model"]
+        '/api/<string:model>/<int:rec_id>',
+        auth='public', methods=['GET'], csrf=False)
+    def get_model_rec(self, model, rec_id, **params):
+        records = request.env[model].sudo().search([])
+        if "query" in params:
+            query = json.loads(params["query"])
+        else:
+            query = records.fields_get_keys()
+        
+        if "exclude" in params:
+            exclude = json.loads(params["exclude"])
+            for field in exclude:
+                if field in query[0]:
+                    field_to_exclude= query[0].index(field)
+                    query[0].pop(field_to_exclude)
+
+        record = records.browse(rec_id).ensure_one()
+        data = dictfier.dictfy(
+            record,
+            query,
+            flat_obj=flat_obj,
+            nested_flat_obj=nested_flat_obj,
+            nested_iter_obj=nested_iter_obj
+        )
+        return http.Response(
+            json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
+
+    @http.route(
+        '/api/<string:model>/', 
+        type='json', auth="public", 
+        methods=['POST'], website=True, csrf=False)
+    def post_model_data(self, model, **post):
         if "context" in post:
-            data = request.env[model].with_context(**post["context"]).sudo().create(post["data"])
+            data = request.env[model].with_context(**post["context"])\
+                   .sudo().create(post["data"])
         else:
             data = request.env[model].sudo().create(post["data"])
         return data.id
 
     @http.route(
-        '/api/', 
-        type='json', auth="public", methods=['PUT'], website=True, csrf=False)
-    def put_model_data(self, **post):
-        model = post["model"]
+        '/api/<string:model>', 
+        type='json', auth="public", 
+        methods=['PUT'], website=True, csrf=False)
+    def put_model_data(self, model, **post):
         filters = post["filter"]
         rec = request.env[model].sudo().search(filters)
 
         if "context" in post:
-            rec = request.env[model].with_context(**post["context"]).sudo().search(filters)
+            rec = request.env[model].with_context(**post["context"])\
+                  .sudo().search(filters)
         else:
             rec = request.env[model].sudo().search(filters)
 
@@ -141,10 +175,10 @@ class OdooAPI(http.Controller):
             return False
 
     @http.route(
-        '/api/', 
-        type='http', auth="public", methods=['DELETE'], website=True, csrf=False)
-    def delete_model_data(self, **post):
-        model = post["model"]
+        '/api/<string:model>/', 
+        type='http', auth="public", 
+        methods=['DELETE'], website=True, csrf=False)
+    def delete_model_data(self, model, **post):
         filters = json.loads(post["filter"])
         rec = request.env[model].sudo().search(filters)
         if rec.exists():
