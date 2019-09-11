a2f771079cf986821bee070cec72d4e76a452aca
yezy <yezileliilomo@hotmail.com>
2019-09-11 16:16:41 +0300

    :bug:Fix module not showing up on app list
    Fixes #13

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'developers',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,7 +32,11 @@
         'demo/demo.xml',
     ],
 
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+
     'external_dependencies': {
-        'python': ['dictfier']
+        'python': ['dictfier', 'requests']
     }
 }
