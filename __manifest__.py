fdfca56e6e5079ccf9b39b4d6bd79f06a929b24d
yezy <yezileliilomo@hotmail.com>
2020-06-25 22:36:27 +0300

    🔧Update manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'author': "Yezileli Ilomo",
-    'website': "http://www.singo.africa",
+    'website': "https://github.com/yezyilomo/odoo-rest-api",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
